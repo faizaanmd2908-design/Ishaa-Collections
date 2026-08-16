5d6f9cdcf1ed14752cd574909f996400b22c9f4b
Mohammad Faizaan <faizaanmd2908@gmail.com>
2026-08-16 13:43:25 +0530

    Fix Aiven MySQL SSL connection

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
 
 const mysql = require('mysql2');
+const fs = require('fs');
+const path = require('path');
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     ssl: {
-        rejectUnauthorized: false
+        ca: fs.readFileSync(path.join(__dirname, 'ca.pem')),
+        rejectUnauthorized: true
     }
 });
 
